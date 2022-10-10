51d21aea0deb036ff5240f09896a4e4aa6d4f5d7
watanabe0055 <mailok1212+github@gmail.com>
2022-10-11 06:22:37 +0900

    日記の一覧apiを作成

--- app/controllers/api/v1/diary_controller.rb
+++ app/controllers/api/v1/diary_controller.rb
@@ -3,12 +3,15 @@ module Api
     class DiaryController < ApplicationController
 
       def index
-          #allDairy = Diary.joins(:user).where(is_deleted: false).order(created_at: "desc")
-          #render status: 200, json: { diary: allDairy}
           if current_api_v1_user
             user = current_api_v1_user.id
-            allDairy = Diary.joins(:user).where(user_id: user,is_deleted: false).order(created_at: "desc")
-            render status: 200, json: { diary: allDairy}
+            allDairy = Diary.joins(:user).select('id','emotion_id','diary_hashtag_id','title','content').where(user_id: user,is_deleted: false).order(created_at: "desc")
+            if allDairy.length > 1
+              render status: 200, json: { diary: allDairy}
+            else
+              render json: { is_login: false, message: "日記が存在しません" }
+            end
+            
           else
             render json: { is_login: false, message: "ユーザーが存在しません" }
           end
