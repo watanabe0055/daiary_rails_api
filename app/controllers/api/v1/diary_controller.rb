ab1941563147894539561278c85b30a8604125f8
watanabe0055 <mailok1212+github@gmail.com>
2022-10-11 06:57:12 +0900

    日記詳細apiを作成

--- app/controllers/api/v1/diary_controller.rb
+++ app/controllers/api/v1/diary_controller.rb
@@ -11,14 +11,27 @@ module Api
             else
               render json: { is_login: false, message: "日記が存在しません" }
             end
-            
           else
             render json: { is_login: false, message: "ユーザーが存在しません" }
           end
-        
-        
       end
       
+      def show
+        if current_api_v1_user
+          diary = Diary.find(params[:id])
+          #diaryとuserでidの型が違うから、to_sで合わせてます
+          if diary.id.blank? == false && diary.is_deleted == false && diary.user_id == current_api_v1_user.id.to_s
+            render status: 200, json: { diary: diary}
+          elsif diary.is_deleted == true
+            render status: 400, json: { status: 'deleted_diary_data', message: '削除済みのデータです' }
+          else
+            render status: 400, json: { status: 'deleted_diary_data', message: '閲覧権限のないデータです' }
+          end
+        else
+          render json: { is_login: false, message: "ユーザーが存在しません" }
+        end
+      end
+
     end
   end
 end
