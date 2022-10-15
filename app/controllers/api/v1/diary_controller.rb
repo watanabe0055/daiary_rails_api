4c3397808f863cef84af64b37065f5f659d95c5f
watanabe0055 <mailok1212+github@gmail.com>
2022-10-16 05:20:10 +0900

    日記投稿 APIを実装"
    ；’
    end

--- app/controllers/api/v1/diary_controller.rb
+++ app/controllers/api/v1/diary_controller.rb
@@ -1,27 +1,39 @@
 module Api
   module V1
     class DiaryController < ApplicationController
+      #日記投稿API
+      def create
+        if current_api_v1_user
+          newDiary = Diary.new(post_diary_params)
+          if newDiary.save
+            render status: 200, json: { status: 'SUCCESS', message: 'Created the post', data: newDiary }
+          else
+            render status: 400, json: { status: 'Falled Error', message: '日記の保存に失敗しました'}
+          end
+        else
+          render json: { status: 'Not Loggend in', message: "ログインしてください" }
+        end
+      end
 
       #日記一覧API
       def index
-          if current_api_v1_user
-            user = current_api_v1_user.id
-            allDairy = Diary.joins(:user).select('id','user_id','emotion_id','diary_hashtag_id','title','content').where(user_id: user,is_deleted: false).order(created_at: "desc")
-            if allDairy.length > 1
-              render status: 200, json: { diary: allDairy}
-            else
-              render json: { status: 'Failure Get Diary Data', message: "日記が存在しません" }
-            end
+        if current_api_v1_user
+          user = current_api_v1_user.id
+          allDairy = Diary.joins(:user).select('id','user_id','emotion_id','diary_hashtag_id','title','content').where(user_id: user,is_deleted: false).order(created_at: "desc")
+          if allDairy.length > 1
+            render status: 200, json: { diary: allDairy}
           else
-            render json: { status: 'Not Loggend in', message: "ログインしてください" }
+            render json: { status: 'Failure Get Diary Data', message: "日記が存在しません" }
           end
+        else
+          render json: { status: 'Not Loggend in', message: "ログインしてください" }
+        end
       end
       
       #日記詳細API
       def show
         #存在しないレコードの時に、nilを返したい為「find_by」を使ってる
         diary = Diary.find_by(id: params[:id])
-
         if current_api_v1_user
           if diary == nil
             render status: 400, json: { status: 'not_exist_diary_data', message: '存在しないレコードです' }
@@ -81,7 +93,7 @@ module Api
 
       private
         def post_diary_params
-          params.permit(:user_id, :title, :content, :emotion_id)
+          params.permit(:title, :content, :emotion_id).merge(user_id: current_api_v1_user.id)
         end
 
         def post_edit_diary_params
