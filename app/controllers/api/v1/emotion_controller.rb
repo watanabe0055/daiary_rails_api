c9b50f2e2e6abe4841ca76f4628205f976ac48a4
watanabe0055 <mailok1212+github@gmail.com>
2022-10-16 22:30:22 +0900

    エモーション詳細APIの実装

--- app/controllers/api/v1/emotion_controller.rb
+++ app/controllers/api/v1/emotion_controller.rb
@@ -14,6 +14,24 @@ module Api
         end
       end
 
+      def show
+        #存在しないレコードの時に、nilを返したい為「find_by」を使ってる
+        emotion = Emotion.find_by(id: params[:id])
+        if current_api_v1_user
+          if emotion == nil
+            render status: 400, json: { status: 'not_exist_emotion_data', message: '存在しないレコードです' }
+          #emotionとuserでidの型が違うから、to_sで合わせてます
+          elsif emotion.is_deleted == false
+            render status: 200, json: { emotion: emotion}
+          elsif emotion.is_deleted == true
+            render status: 400, json: { status: 'deleted_emotion_data', message: '削除済みのデータです' }
+          else
+            render status: 400, json: { status: 'Erroy', message: '例外処理' }
+          end
+        else
+          render json: { status: 'Not Loggend in', message: "ログインしてください" }
+        end
+      end
 
     end
   end
