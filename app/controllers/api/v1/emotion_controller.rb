2ac1f32c27e75ae9480a18e504922959ec076829
watanabe0055 <mailok1212+github@gmail.com>
2022-10-16 22:20:03 +0900

    エモーション一覧の例外処理を追加

--- app/controllers/api/v1/emotion_controller.rb
+++ app/controllers/api/v1/emotion_controller.rb
@@ -2,9 +2,19 @@ module Api
   module V1
     class EmotionController < ApplicationController
       def index
-        emotionAll = Emotion.all.where(is_deleted: false).select(:id,:emotion)
-        render json: { status: 'SUCCESS', emotionAll: emotionAll }
+        emotionAll = Emotion.all.where(is_deleted: false).select(:id,:emotion).order(created_at: "desc")
+        if current_api_v1_user
+          if emotionAll.length > 1
+            render json: { status: 'SUCCESS', emotionAll: emotionAll }
+          else
+            render json: { status: 'Failure Get Emotion Data', message: "エモーションが存在しません" }
+          end
+        else
+          render json: { status: 'Not Loggend in', message: "ログインしてください" }
+        end
       end
+
+
     end
   end
 end
