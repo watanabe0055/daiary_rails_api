c1631db5c2eb230d04fe7fdbfa6e44cb882dded6
watanabe0055 <mailok1212+github@gmail.com>
2022-12-21 00:16:20 +0900

    diaryのバリデーションを入れました close #12

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,7 @@
 class Diary < ApplicationRecord
   belongs_to :user
   belongs_to :emotion
+  #バリデーション定義
+  validates :title,    length: { in: 1..75 }
+  validates :content,    length: { in: 1..4000 }
 end
